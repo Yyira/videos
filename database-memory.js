1808ddefa407d486539278bbf70bd696a6711e9f
icaro <irenanlima16@gmail.com>
2024-08-12 15:46:37 -0300

    videos_1.0

--- database-memory.js
+++ database-memory.js
@@ -1,11 +1,42 @@
-export class DatabaseMemory {
-    #videos = []
+// UUID =  Universally Unique IDentifier
+import { randomUUID } from "node:crypto"
 
+export class DatabaseMemory {
+    #videos = new Map() 
+    
+
+    list(search) {
+        return Array.from(this.#videos.entries())
+        .map
+        ((videoArray) =>{
+            const id = videoArray[0]
+            const data = videoArray [1]
+
+            return {
+                id,
+                ...data, 
+            }
+        })
+        .filter(video => {
+            if (search){
+                return video.title.includes(search)
+            }
+
+            return true
+        })
+
+    }
     create(video) {
-        this.#videos.push(video)
+        const videoId = randomUUID()
+
+        this.#videos.set(videoId, video)
     }
 
     update(id, video) {
-        this.#videos.push(video)
+        this.#videos.set(id, video)
     }
+    delete(id) {
+        this.#videos.set(id)
+    }
+
 }
--- server.js
+++ server.js
@@ -12,23 +12,59 @@ import { fastify } from "fastify";
 import { DatabaseMemory } from "./database-memory.js";
 const server = fastify()
 
-server.post ('/videos', ()=> {
-    return 'hello World'
+const database = new DatabaseMemory()
+
+// request body
+
+
+server.post ('/videos', (request, reply)=> {
+    const { title, description, duration} = request.body
+
+  
+    database.create({
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(201).send()
+    //status_201 "algo foi criado"
 })
 
-server.get ('/videos', ()=> {
-    return 'hello World'
+server.get ('/videos', (request)=> {
+    const search = request.query.search
+
+    console.log(search)
+
+
+    const videos = database.list(search)
+    
+
+
+    return videos
 })
 
-server.put ('/videos/:id', ()=> {
-    return 'hello World'
+server.put ('/videos/:id', (request, reply)=> {
+    const videoId = request.params.id
+    const { title, description, duration } = request.body
+    database.update(videoId, {
+        title,
+        description,
+        duration
+    })
+
+    return reply.status(204).send()
 })
 
-server.delete ('/videos/:id', ()=> {
-    return 'hello World'
+server.delete ('/videos/:id', (request, reply)=> {
+    const videoId = request.params.id
+
+    database.delete(videoId)
+
+    return reply.status(204).send()
 })
 
 
 server.listen(
-    {port:3333}
+    {port:3332}
 )
